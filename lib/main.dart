9ba4b039335a67b06cf750dc840712f5b623ffb6
penne <penne0505pp@gmail.com>
2024-12-05 16:37:38 +0900

    #

--- lib/main.dart
+++ lib/main.dart
@@ -46,14 +46,39 @@ class WeekViewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         // child: WeekGrid(dayInfos: [tempDayInfo])
-        child: Padding(
-          padding: EdgeInsets.only(top: 12.0),
-          child: WeekGrid(),
+        // child: Padding(
+        //   padding: EdgeInsets.only(top: 12.0),
+        //   child: WeekGrid(),
+        child:Container(
+          width: 200,
+          height: 200,
+          alignment: Alignment.center,
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xfff9f9ff),
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: [
+                const BoxShadow(
+                  color: Color(0x7fa0c4ff),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(3, 3),
+                ),
+                const BoxShadow(
+                  color: Color(0xffffffff),
+                  spreadRadius: 4,
+                  blurRadius: 6,
+                  offset: Offset(-3, -3),
+
+                )
+              ],
+            ),
+          ),
         )
-      ),
+        )
     );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class LogidianTheme {
   static ThemeData get theme {
     return ThemeData(
-      primaryColor: const Color(0xff774dff),
+      primaryColor: const Color(0xff27A0C8),
       scaffoldBackgroundColor: const Color(0xffffffff),
       dividerColor: const Color(0xffeeeeee),
       shadowColor: const Color(0x33000000),
@@ -18,9 +18,9 @@ class LogidianTheme {
 
 /// 色を参照する時に使う(テーマで自動配色できなかったところなどに)
 class SpecifyColors {
-  static const Color primary = Color(0xff774dff);
-  static const Color attention = Color(0xFFCC4536);
-  static const Color success = Color(0xFF399D70);
+  static const Color primary = Color(0xff27A0C8);
+  static const Color attention = Color(0xFFFF6B6B);
+  static const Color success = Color(0xFF8AC926);
   static const Color white = Color(0xffffffff);
   static const Color black = Color(0xff1e1e1e);
   static const Color thinGrey = Color(0xfff9f9f9);
@@ -32,9 +32,11 @@ class SpecifyColors {
 
 
 /*
-primary: 0xff774dff // メインカラー
-attention: 0xFFCC4536 // 警告色(通知、エラーとか)
-success: 0xFF399D70 // 成功色(通知、エラーとか)
+primary: 0xff27A0C8 // メインカラー
+primary_dark: 0xff225B6D // メインカラーの濃い色(白テキストとかを乗せるときに使う)
+secondary: 0xffFFCA3A // セカンダリカラー(よっぽどのことがない限り使わない)
+attention: 0xFFFF6B6B // 警告色(通知、エラーとか)
+success: 0xFF8AC926 // 成功色(通知、エラーとか)
 white: 0xffffffff // 背景色とか
 black: 0xff1e1e1e // 強調するテキスト、選択中のテキストとか
 thinGrey: 0xfffbfbfb // コンテナの背景色とか
@@ -42,5 +44,5 @@ grey: 0xffeeeeee // 薄い線とか
 tickGrey: 0xff666666 // テキストとか
 shadow: 0x33000000 // 影
 overlay: 0x66000000 // オーバーレイ
-link: 0xFF525AF4 // リンク
+link: 0xFF0069D8 // リンク色
  */
